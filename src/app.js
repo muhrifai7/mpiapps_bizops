c85bcb235b585fcc5af4570728aea2c4a524ede2
muh rifai <muhrifai554@gmail.com>
2023-08-06 11:31:40 +0700

    test

--- src/app.js
+++ src/app.js
@@ -28,7 +28,19 @@ app.listen(port, () => {
     console.log("run test");
     // make sure that any items are correctly URL encoded in the connection string
     await mssql.connect(configSqlServer);
-    const result = await mssql.query`select * from DMS_SD_Route`;
+    const result = await mssql.query`SELECT
+    a.szId AS kode_rayon,
+    a.szName AS nama_rayon,
+    a.szEmployeeId AS kode_sales,
+    c.szName AS nama_sales,
+    b.szCustomerId AS kode_customer,
+    d.szName AS nama_customer
+  FROM DMS_SD_Route a
+  INNER JOIN DMS_SD_RouteItem b ON a.szId = b.szId
+  LEFT JOIN DMS_PI_Employee c ON a.szEmployeeId = c.szId
+  LEFT JOIN DMS_AR_Customer d ON b.szCustomerId = d.szId
+  ORDER BY a.szId;
+  `;
     console.log("test", result);
   } catch (err) {
     console.log(err, "err");
