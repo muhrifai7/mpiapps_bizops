744c97a671ef89f6e1cb365119687f86563f60d9
muh rifai <muhrifai554@gmail.com>
2023-08-06 11:30:32 +0700

    test

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 import express from "express";
 import routes from "./api/routes/index.js";
 import dotenv from "dotenv";
+import mssql from "mssql";
+
+import { configSqlServer, configSqlServerLocal } from "./config/db.js";
 
 dotenv.config();
 const app = express();
@@ -19,3 +22,16 @@ app.use("/api", routes);
 app.listen(port, () => {
   console.log(`Server is running on ${process.env.ENV}:${port}`);
 });
+
+(async () => {
+  try {
+    console.log("run test");
+    // make sure that any items are correctly URL encoded in the connection string
+    await mssql.connect(configSqlServer);
+    const result = await mssql.query`select * from DMS_SD_Route`;
+    console.log("test", result);
+  } catch (err) {
+    console.log(err, "err");
+    // ... error checks
+  }
+})();
