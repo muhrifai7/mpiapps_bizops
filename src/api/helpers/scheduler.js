383f4ea022e57489a33b7ed4244a910b5e5e4949
muh rifai <muhrifai554@gmail.com>
2023-08-10 00:10:30 +0700

    add

--- src/api/helpers/scheduler.js
+++ src/api/helpers/scheduler.js
@@ -79,23 +79,57 @@ const importDataPriceListToSimpi = async () => {
   const batchSize = 1000;
   await mssql.connect(configSqlServer);
   // select data rayon from sql
-  const query = `select a.iInternalId as iInternalIdA, a.iId as iIdA, a.szId as szIdA, a.szName, a.szDescription, a.szCombinationId as szCombinationIdA, a.szCompanyId, a.dtmValidFrom, a.dtmValidTo,
-                  a.intPriority, a.bActive, a.szUserCreatedId, a.szUserUpdatedId, a.dtmCreated, a.dtmLastUpdated, a.szStatusSubmitFusion,
-                  b.iInternalId as iInternalIdB, b.iId as iIdB, b.szId as szIdC, b.intItemNumber as intItemNumberB, b.szCombinationId, b.szCombinationValue, b.szCombinationValueNm,
-                  c.iInternalId as iInternalIdB, c.iId as iIdC, c.szId as szIdC, c.intItemNumber as intItemNumberC, c.intItemNumber2, c.szProductId, c.decMinQty, c.bIncludeTax, c.decPrice, c.szUomId,
-                  c.intLine, c.dtmStartDate, c.dtmEndDate, c.szPriceListId, c.szPriceListItemId, c.szPriceListChargeId
-                  from dms_sd_pricecatalog a
-                  inner join DMS_SD_PriceCatalogItemCombinationValue b on a.szid = b.szid
-                  inner join DMS_SD_PriceCatalogItemCombinationValueItem c on b.szid = c.szid
-                  WHERE c.dtmStartDate <=  GETDATE()
-                  AND c.dtmEndDate >=  GETDATE()
+  const query = `SELECT a.iInternalId AS iInternalId
+                    ,a.iId AS iIdA
+                    ,a.szId AS szIdA
+                    ,a.szName
+                    ,a.szDescription
+                    ,a.szCombinationId AS szCombinationIdA
+                    ,a.szCompanyId
+                    ,a.dtmValidFrom
+                    ,a.dtmValidTo
+                    ,a.intPriority
+                    ,a.bActive
+                    ,a.szUserCreatedId
+                    ,a.szUserUpdatedId
+                    ,a.dtmCreated
+                    ,a.dtmLastUpdated
+                    ,a.szStatusSubmitFusion
+                    ,b.iInternalId AS iInternalIdB
+                    ,b.iId AS iIdB
+                    ,b.szId AS szIdB
+                    ,b.intItemNumber AS intItemNumberB
+                    ,b.szCombinationId AS szCombinationIdB
+                    ,b.szCombinationValue
+                    ,b.szCombinationValueNm
+                    ,c.iInternalId AS iInternalIdC
+                    ,c.iId AS iIdC
+                    ,c.szId AS szIdC
+                    ,c.intItemNumber AS intItemNumberC
+                    ,c.intItemNumber2
+                    ,c.szProductId
+                    ,c.decMinQty
+                    ,c.bIncludeTax
+                    ,c.decPrice
+                    ,c.szUomId
+                    ,c.intLine
+                    ,c.dtmStartDate
+                    ,c.dtmEndDate
+                    ,c.szPriceListId
+                    ,c.szPriceListItemId
+                    ,c.szPriceListChargeId
+                  FROM dms_sd_pricecatalog a
+                  INNER JOIN DMS_SD_PriceCatalogItemCombinationValue b ON a.szid = b.szid
+                  INNER JOIN DMS_SD_PriceCatalogItemCombinationValueItem c ON b.szid = c.szid
+                  WHERE c.dtmStartDate <= GETDATE()
+                    AND c.dtmEndDate >= GETDATE()
                 `;
 
   const poolToSimpi = await getPoolToSimpiTest();
   try {
     const result = await mssql.query(query);
     const data = result.recordset;
-    console.log(data, "dataa");
+    console.log(data, "data");
     if (data.length > 0) {
       await poolToSimpi.query("START TRANSACTION");
       const truncateQuery = `TRUNCATE TABLE price_list`;
@@ -108,11 +142,11 @@ const importDataPriceListToSimpi = async () => {
       for (const chunk of chunks) {
         const values = chunk.map((data) => [
           data?.iInternalId,
-          data?.iId,
-          data?.szId,
+          data?.iIdA,
+          data?.szIdA,
           data?.szName,
           data?.szDescription,
-          data?.szCombinationId,
+          data?.szCombinationIdA,
           data?.szCompanyId,
           data?.dtmValidFrom,
           data?.dtmValidTo,
@@ -123,17 +157,17 @@ const importDataPriceListToSimpi = async () => {
           data?.dtmCreated,
           data?.dtmLastUpdated,
           data?.szStatusSubmitFusion,
-          data?.iInternalId_1,
-          data?.iId_1,
-          data?.szId_1,
-          data?.intItemNumber,
-          data?.szCombinationId_1,
+          JSON.stringify(data?.iInternalIdB),
+          data?.iIdB,
+          data?.szIdB,
+          data?.intItemNumberB,
+          data?.szCombinationIdB,
           data?.szCombinationValue,
           data?.szCombinationValueNm,
-          data?.iInternalId_2,
-          data?.iId_2,
-          data?.szId_2,
-          data?.intItemNumber_1,
+          data?.iInternalIdC,
+          data?.iIdC,
+          JSON.stringify(data?.szIdC),
+          data?.intItemNumberC,
           data?.intItemNumber2,
           data?.szProductId,
           data?.decMinQty,
@@ -150,16 +184,46 @@ const importDataPriceListToSimpi = async () => {
 
         // query insert only
         const insertQuery = `
-          INSERT INTO price_list (
-              iInternalId, iId, szId, szName, szDescription, szCombinationId, szCompanyId,
-              dtmValidFrom, dtmValidTo, intPriority, bActive, szUserCreatedId, szUserUpdatedId,
-              dtmCreated, dtmLastUpdated, szStatusSubmitFusion,
-              iInternalId_1, iId_1, szId_1, intItemNumber, szCombinationId_1,
-              szCombinationValue, szCombinationValueNm,
-              iInternalId_2, iId_2, szId_2, intItemNumber_1, intItemNumber2,
-              szProductId, decMinQty, bIncludeTax, decPrice, szUomId,
-              intLine, dtmStartDate, dtmEndDate, szPriceListId,
-              szPriceListItemId, szPriceListChargeId
+        INSERT INTO price_list (
+          iInternalId
+          ,iId
+          ,szId
+          ,szName
+          ,szDescription
+          ,szCombinationId
+          ,szCompanyId
+          ,dtmValidFrom
+          ,dtmValidTo
+          ,intPriority
+          ,bActive
+          ,szUserCreatedId
+          ,szUserUpdatedId
+          ,dtmCreated
+          ,dtmLastUpdated
+          ,szStatusSubmitFusion
+          ,iInternalId_1
+          ,iId_1
+          ,szId_1
+          ,intItemNumber
+          ,szCombinationId_1
+          ,szCombinationValue
+          ,szCombinationValueNm
+          ,iInternalId_2
+          ,iId_2
+          ,szId_2
+          ,intItemNumber_1
+          ,intItemNumber2
+          ,szProductId
+          ,decMinQty
+          ,bIncludeTax
+          ,decPrice
+          ,szUomId
+          ,intLine
+          ,dtmStartDate
+          ,dtmEndDate
+          ,szPriceListId
+          ,szPriceListItemId
+          ,szPriceListChargeId
           )
           VALUES (
               ?, ?, ?, ?, ?, ?, ?,
@@ -173,8 +237,10 @@ const importDataPriceListToSimpi = async () => {
               ?, ?
           );
           `;
+        const flattenedValues = values.flat();
 
-        const execQuery = await poolToSimpi.query(insertQuery, [values]);
+        console.log([flattenedValues], "values");
+        const execQuery = await poolToSimpi.query(insertQuery, flattenedValues);
         console.log(
           "Batch inserted. Row(s) affected:",
           execQuery[0]?.affectedRows
